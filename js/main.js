737d77933c130b0947b17327249c5d7b56a9f2dd
Joanthan Chosson <catstrike@gmail.com>
2020-12-04 17:19:23 +0100

    [JS]main.js :construction: Creation du JS

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.getElementById('ville').addEventListener('change', function(){
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${this.value}&units=metric&appid=4802d916ef4ea849a434c682b46cdb10&lang=fr`;
+    fetch(url)
+        .then((reponse) => 
+        reponse.json()
+        .then((data) => {
+            console.log(data);
+            let tab = document.querySelectorAll('ul li');
+            let i = 0;
+            tab[i].innerHTML = tab[i].innerHTML.replace(tab[i].innerText, data.name);
+            i += 1;
+            tab[i].innerHTML = tab[i].innerHTML.replace(tab[i].innerText, data.main.temp) + "°";
+            i +=1;
+            tab[i].innerHTML = tab[i].innerHTML.replace(tab[i].innerText, data.main.humidity) + " %";
+            i+=1;
+            tab[i].innerHTML = tab[i].innerHTML.replace(tab[i].innerText, data.wind.speed) + " km/h";
+        })
+        ).catch(erreur => console.log('Ville non trouvé'));
+});
